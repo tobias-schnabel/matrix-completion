86bfc82a4a1ee804551f1850f921475049147aee
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-05-01 20:11:16 +0200

    calling new package script

--- Setup.R
+++ Setup.R
@@ -1,25 +1,9 @@
 ##CLEAR ALL
 rm(list = ls(all = TRUE)) 
-
-# Package names
-packages <- c("tidyverse", "xtable", "stargazer", "purrr", "callr", "ggplot2",
-              "panelView", "devtools", "latex2exp", 
-              "gsynth", "did2s", "DIDmultiplegt", "fixest", "DRDID", "staggered")
-
-# comment in and execute to install CRAN packages
-# installed_packages <- packages %in% rownames(installed.packages())
-# if (any(installed_packages == FALSE)) {
-#   install.packages(packages[!installed_packages])
-# }
-
 # load packages
-invisible(lapply(packages, library, character.only = T))
+source("packages.R")
 
-# comment in and execute to install MC packages by Susan Athey
-# devtools::install_github("susanathey/MCPanel", "synth-inference/synthdid")
-library(synthdid)
-library(MCPanel)
-library(devtools, latex2exp, panelView)
+
 
 # Run Main Scripts
 rscript("Functions.R")
