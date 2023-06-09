05b6d31823089fe71926b5c1d14a8506a0f3742e
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-06-09 23:09:03 +0200

    revert last

--- Export.R
+++ Export.R
@@ -23,17 +23,6 @@ grateful::cite_packages(output = "file",
               include.RStudio = T,
               bib.file = "packages")
 
-# Write the package info to HTML file on github
-grateful::cite_packages(output = "file",
-                        out.dir = "~/Git/matrix-completion",
-                        out.format = "pdf",
-                        omit = NULL,
-                        cite.tidyverse = T,
-                        dependencies = T,
-                        include.RStudio = T)
-file.remove("grateful-refs.bib") # cleanup
-file.rename("grateful-report.pdf", "Packages.pdf")
-
 ## Modify all entries to have type "Manual"
 # Read file
 bib_file = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/Thesis/Tex/packages.bib"
