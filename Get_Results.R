d8941a7569be6a5c26368f11a69f10bb67050eca
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-06-16 22:45:47 +0200

    changed iters

--- Get_Results.R
+++ Get_Results.R
@@ -1,7 +1,7 @@
 #### This Script carries out the main analysis
 # Test parallel simulation function: DGP1 took 40 min @ 260 corrupted iters
 par_start = Sys.time()
-dgp1_raw = run_parallel_sim(1:650, dgp_1_sim)
+dgp1_raw = run_parallel_sim(1:700, dgp_1_sim)
 par_end = Sys.time()
 print(paste0("Elapsed time: ", (par_end - par_start)))
 dgp_1 = verify_sim_results(dgp1_raw)
