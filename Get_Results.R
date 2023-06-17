ae58b404e3c74d30badea6167d4a15b5ac8af542
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-06-17 16:57:07 +0200

    set up all simulations

--- Get_Results.R
+++ Get_Results.R
@@ -1,65 +1,94 @@
 #### This Script carries out the main analysis
-# Test parallel simulation function: DGP1 took 60 min @ 350 corrupted iters
-sim1_start = Sys.time()
-dgp1_raw = run_parallel_sim(1:500, dgp_1_sim)
-sim1__end = Sys.time()
-print(paste0("Elapsed time: ", (sim1_end - sim1_start)))
-dgp_1 = verify_sim_results(dgp1_raw)
-verify_iteration_counts(dgp_1)
-save_sim_results(dgp_1)
+
+## Simulate using purrr:map (slower but more reliable in terms of corrupted 
+#iterations)
+
+# DGP 1
+sim1_out = run_sim_map(1:500, dgp_1_sim)
+sim1 = verify_sim_results(sim1_out)
+verify_iteration_counts(sim1)
+save_sim_results(sim1, "Sim_1")
 
 # DGP 2
-sim2_start = Sys.time()
-dgp2_raw = run_parallel_sim(1:500, dgp_2_sim)
-sim2_end = Sys.time()
-print(paste0("Elapsed time: ", (sim2_end - sim2_start)))
-dgp_2 = verify_sim_results(dgp2_raw)
-verify_iteration_counts(dgp_2)
-save_sim_results(dgp_2)
+sim2_out = run_sim_map(1:500, dgp_2_sim)
+sim2 = verify_sim_results(sim2_out)
+verify_iteration_counts(sim2)
+save_sim_results(sim2, "Sim_2")
 
 # DGP 3
-sim3_start = Sys.time()
-dgp3_raw = run_parallel_sim(1:500, dgp_3_sim)
-sim3_end = Sys.time()
-print(paste0("Elapsed time: ", (sim3_end - sim3_start)))
-dgp_3 = verify_sim_results(dgp3_raw)
-verify_iteration_counts(dgp_3)
-save_sim_results(dgp_3)
+sim3_out = run_sim_map(1:500, dgp_3_sim)
+sim3 = verify_sim_results(sim3_out)
+verify_iteration_counts(sim3)
+save_sim_results(sim3, "Sim_3")
 
 # DGP 4
-sim4_start = Sys.time()
-dgp4_raw = run_parallel_sim(1:500, dgp_4_sim)
-sim4_end = Sys.time()
-print(paste0("Elapsed time: ", (sim4_end - sim4_start)))
-dgp_4 = verify_sim_results(dgp4_raw)
-verify_iteration_counts(dgp_4)
-save_sim_results(dgp_4)
+sim4_out = run_sim_map(1:500, dgp_4_sim)
+sim4 = verify_sim_results(sim4_out)
+verify_iteration_counts(sim4)
+save_sim_results(sim4, "Sim_4")
 
 # DGP 5
-sim5_start = Sys.time()
-dgp5_raw = run_parallel_sim(1:500, dgp_5_sim)
-sim5_end = Sys.time()
-print(paste0("Elapsed time: ", (sim5_end - sim5_start)))
-dgp_5 = verify_sim_results(dgp5_raw)
-verify_iteration_counts(dgp_5)
-save_sim_results(dgp_5)
+sim5_out = run_sim_map(1:500, dgp_5_sim)
+sim5 = verify_sim_results(sim5_out)
+verify_iteration_counts(sim5)
+save_sim_results(sim5, "Sim_5")
 
 # DGP 6
-sim6_start = Sys.time()
-dgp6_raw = run_parallel_sim(1:500, dgp_6_sim)
-sim6_end = Sys.time()
-print(paste0("Elapsed time: ", (sim6_end - sim6_start)))
-dgp_6 = verify_sim_results(dgp6_raw)
-verify_iteration_counts(dgp_6)
-save_sim_results(dgp_6)
+sim6_out = run_sim_map(1:500, dgp_6_sim)
+sim6 = verify_sim_results(sim6_out)
+verify_iteration_counts(sim6)
+save_sim_results(sim6, "Sim_6")
 
 # DGP 7
-sim7_start = Sys.time()
-dgp7_raw = run_parallel_sim(1:500, dgp_7_sim)
-sim7__end = Sys.time()
-print(paste0("Elapsed time: ", (sim7_end - sim7_start)))
-dgp_7 = verify_sim_results(dgp7_raw)
-verify_iteration_counts(dgp_7)
-save_sim_results(dgp_7)
+sim7_out = run_sim_map(1:500, dgp_7_sim)
+sim7 = verify_sim_results(sim7_out)
+verify_iteration_counts(sim7)
+save_sim_results(sim7, "Sim_7")
+
+
+## Simulate using mclapply(substantially faster but less reliable in terms of corrupted 
+#iterations)
+
+# DGP 1
+sim1_par_out = run_sim_parallel(1:1000, dgp_1_sim)
+sim1_par = verify_sim_results(sim1_par_out)
+verify_iteration_counts(sim1_par)
+save_sim_results(sim1_par, "Sim_1_par")
+
+# DGP 2
+sim2_par_out = run_sim_parallel(1:1000, dgp_2_sim)
+sim2_par = verify_sim_results(sim2_par_out)
+verify_iteration_counts(sim2_par)
+save_sim_results(sim2_par, "Sim_2_par")
+
+# DGP 3
+sim3_par_out = run_sim_parallel(1:1000, dgp_3_sim)
+sim3_par = verify_sim_results(sim3_par_out)
+verify_iteration_counts(sim3_par)
+save_sim_results(sim3_par, "Sim_3_par")
+
+# DGP 4
+sim4_par_out = run_sim_parallel(1:1000, dgp_4_sim)
+sim4_par = verify_sim_results(sim4_par_out)
+verify_iteration_counts(sim4_par)
+save_sim_results(sim4_par, "Sim_4_par")
+
+# DGP 5
+sim5_par_out = run_sim_parallel(1:1000, dgp_5_sim)
+sim5_par = verify_sim_results(sim5_par_out)
+verify_iteration_counts(sim5_par)
+save_sim_results(sim5_par, "Sim_5_par")
+
+# DGP 6
+sim6_par_out = run_sim_parallel(1:1000, dgp_6_sim)
+sim6_par = verify_sim_results(sim6_par_out)
+verify_iteration_counts(sim6_par)
+save_sim_results(sim6_par, "Sim_6_par")
+
+# DGP 7
+sim7_par_out = run_sim_parallel(1:1000, dgp_7_sim)
+sim7_par = verify_sim_results(sim7_par_out)
+verify_iteration_counts(sim7_par)
+save_sim_results(sim7_par, "Sim_7_par")
 
 
