38dfa646151098183cc94baf5055589cdca6adb5
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-12-28 10:37:51 +0100

    changed iteration counts

--- Get_Results.R
+++ Get_Results.R
@@ -19,59 +19,55 @@ if (sim_bool == 0) {
   # but also more dropped iterations
   
   # DGP 1
-  sim1_par_out = run_sim_parallel(1:600, dgp_1_sim)
+  sim1_par_out = run_sim_parallel(1:550, dgp_1_sim)
   sim1_par = verify_sim_results(sim1_par_out)
   verify_iteration_counts(sim1_par)
   save_sim_results(sim1_par, "Sim_1")
   
   # DGP 2
-  sim2_par_out = run_sim_parallel(1:600, dgp_2_sim)
+  sim2_par_out = run_sim_parallel(1:550, dgp_2_sim)
   sim2_par = verify_sim_results(sim2_par_out)
   verify_iteration_counts(sim2_par)
   save_sim_results(sim2_par, "Sim_2")
   
   # DGP 3
-  sim3_par_out = run_sim_parallel(1:600, dgp_3_sim)
+  sim3_par_out = run_sim_parallel(1:550, dgp_3_sim)
   sim3_par = verify_sim_results(sim3_par_out)
   verify_iteration_counts(sim3_par)
   save_sim_results(sim3_par, "Sim_3")
   
   # DGP 4
-  sim4_par_out = run_sim_parallel(1:600, dgp_4_sim)
+  sim4_par_out = run_sim_parallel(1:550, dgp_4_sim)
   sim4_par = verify_sim_results(sim4_par_out)
   verify_iteration_counts(sim4_par)
   save_sim_results(sim4_par, "Sim_4")
   
   # DGP 5
-  sim5_par_out = run_sim_parallel(1:600, dgp_5_sim)
+  sim5_par_out = run_sim_parallel(1:550, dgp_5_sim)
   sim5_par = verify_sim_results(sim5_par_out)
   verify_iteration_counts(sim5_par)
   save_sim_results(sim5_par, "Sim_5")
   
   # DGP 6
-  sim6_par_out = run_sim_parallel(1:650, dgp_6_sim)
+  sim6_par_out = run_sim_parallel(1:600, dgp_6_sim)
   sim6_par = verify_sim_results(sim6_par_out)
   verify_iteration_counts(sim6_par)
   save_sim_results(sim6_par, "Sim_6")
   
   # DGP 7
-  sim7_par_out = run_sim_parallel(1:650, dgp_7_sim)
+  sim7_par_out = run_sim_parallel(1:600, dgp_7_sim)
   sim7_par = verify_sim_results(sim7_par_out)
   verify_iteration_counts(sim7_par)
   save_sim_results(sim7_par, "Sim_7")
   
   # DGP 8
-  sim8_par_out = run_sim_parallel(1:800, dgp_8_sim)
+  sim8_par_out = run_sim_parallel(1:600, dgp_8_sim)
   sim8_par = verify_sim_results(sim8_par_out)
   verify_iteration_counts(sim8_par)
   save_sim_results(sim8_par, "Sim_8")
   
   # load results
   load_all_results()
-  
-  if (Sys.info()[7] == "ts" | Sys.info()[7] == "tobiasschnabel") {
-    pushover("Single-core Simulations complete")
-  }
   
 } else {
   ## Simulate using purrr:map 
@@ -128,11 +124,7 @@ if (sim_bool == 0) {
   
   # load all results
   load_all_results()
-  
-  if (Sys.info()[7] == "ts" | Sys.info()[7] == "tobiasschnabel") {
-    pushover("Single-core Simulations complete")
-  }
-  
+
 }
 
 
