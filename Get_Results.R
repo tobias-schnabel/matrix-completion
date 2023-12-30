c630cf6d3e6962d33f6376dc8712258659bb8a7e
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-12-30 23:21:35 +0100

    addes sims for 20 periods, streamlined script to get results

--- Get_Results.R
+++ Get_Results.R
@@ -1,9 +1,175 @@
 #### This Script carries all simulations
-if (Sys.info()[7] == "ts" | Sys.info()[7] == "tobiasschnabel") {
-  # library(pushoverr)
-  # set_pushover_user(user = "")
-  # set_pushover_app(token = "")
+
+# Function that sets all sim parameters not matter which action user chooses
+sim_all <- function() {
+  ## 20 periods, treatment times treatgroups = c(11,14,16,18) ##
+  # DGP 1
+  sim1_20_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim1_20 = verify_sim_results(sim1_20_out)
+  verify_iteration_counts(sim1_20)
+  save_sim_results(sim1_20, "DGP-1")
+  
+  # DGP 2
+  sim2_20_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim2_20 = verify_sim_results(sim2_20_out)
+  verify_iteration_counts(sim2_20)
+  save_sim_results(sim2_20, "DGP-2")
+  
+  # DGP 3
+  sim3_20_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 5,
+                                 treatgroups = c(11,14,16,18))
+  sim3_20 = verify_sim_results(sim3_20_out)
+  verify_iteration_counts(sim3_20)
+  save_sim_results(sim3_20, "DGP-3")
+  
+  # DGP 4
+  sim4_20_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim4_20 = verify_sim_results(sim4_20_out)
+  verify_iteration_counts(sim4_20)
+  save_sim_results(sim4_20, "DGP-4")
+  
+  # DGP 5
+  sim5_20_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim5_20 = verify_sim_results(sim5_20_out)
+  verify_iteration_counts(sim5_20)
+  save_sim_results(sim5_20, "DGP-5")
+  
+  # DGP 6
+  sim6_20_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim6_20 = verify_sim_results(sim6_20_out)
+  verify_iteration_counts(sim6_20)
+  save_sim_results(sim6_20, "DGP-6")
+  
+  # DGP 7
+  sim7_20_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim7_20 = verify_sim_results(sim7_20_out)
+  verify_iteration_counts(sim7_20)
+  save_sim_results(sim7_20, "DGP-7")
+  
+  # DGP 8
+  sim8_20_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 20,
+                                 treatgroups = c(11,14,16,18))
+  sim8_20 = verify_sim_results(sim8_20_out)
+  verify_iteration_counts(sim8_20)
+  save_sim_results(sim8_20, "DGP-8")
+  
+  ## 50 periods, treatment times treatgroups = c(11,20,30,40) ##
+  # DGP 1
+  sim1_50_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim1_50 = verify_sim_results(sim1_50_out)
+  verify_iteration_counts(sim1_50)
+  save_sim_results(sim1_50, "DGP-1")
+  
+  # DGP 2
+  sim2_50_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim2_50 = verify_sim_results(sim2_50_out)
+  verify_iteration_counts(sim2_50)
+  save_sim_results(sim2_50, "DGP-2")
+  
+  # DGP 3
+  sim3_50_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 5,
+                                 treatgroups = c(11,20,30,40))
+  sim3_50 = verify_sim_results(sim3_50_out)
+  verify_iteration_counts(sim3_50)
+  save_sim_results(sim3_50, "DGP-3")
+  
+  # DGP 4
+  sim4_50_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim4_50 = verify_sim_results(sim4_50_out)
+  verify_iteration_counts(sim4_50)
+  save_sim_results(sim4_50, "DGP-4")
+  
+  # DGP 5
+  sim5_50_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim5_50 = verify_sim_results(sim5_50_out)
+  verify_iteration_counts(sim5_50)
+  save_sim_results(sim5_50, "DGP-5")
+  
+  # DGP 6
+  sim6_50_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim6_50 = verify_sim_results(sim6_50_out)
+  verify_iteration_counts(sim6_50)
+  save_sim_results(sim6_50, "DGP-6")
+  
+  # DGP 7
+  sim7_50_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim7_50 = verify_sim_results(sim7_50_out)
+  verify_iteration_counts(sim7_50)
+  save_sim_results(sim7_50, "DGP-7")
+  
+  # DGP 8
+  sim8_50_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 50,
+                                 treatgroups = c(11,20,30,40))
+  sim8_50 = verify_sim_results(sim8_50_out)
+  verify_iteration_counts(sim8_50)
+  save_sim_results(sim8_50, "DGP-8")
+  
+  
+  ## 100 periods ##
+  # DGP 1
+  sim1_100_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 100)
+  sim1_100 = verify_sim_results(sim1_100_out)
+  verify_iteration_counts(sim1_100)
+  save_sim_results(sim1_100, "DGP-1")
+  
+  # DGP 2
+  sim2_100_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 100)
+  sim2_100 = verify_sim_results(sim2_100_out)
+  verify_iteration_counts(sim2_100)
+  save_sim_results(sim2_100, "DGP-2")
+  
+  # DGP 3
+  sim3_100_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 100)
+  sim3_100 = verify_sim_results(sim3_100_out)
+  verify_iteration_counts(sim3_100)
+  save_sim_results(sim3_100, "DGP-3")
+  
+  # DGP 4
+  sim4_100_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 100)
+  sim4_100 = verify_sim_results(sim4_100_out)
+  verify_iteration_counts(sim4_100)
+  save_sim_results(sim4_100, "DGP-4")
+  
+  # DGP 5
+  sim5_100_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 100)
+  sim5_100 = verify_sim_results(sim5_100_out)
+  verify_iteration_counts(sim5_100)
+  save_sim_results(sim5_100, "DGP-5")
+  
+  # DGP 6
+  sim6_100_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 100)
+  sim6_100 = verify_sim_results(sim6_100_out)
+  verify_iteration_counts(sim6_100)
+  save_sim_results(sim6_100, "DGP-6")
+  
+  # DGP 7
+  sim7_100_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 100)
+  sim7_100 = verify_sim_results(sim7_100_out)
+  verify_iteration_counts(sim7_100)
+  save_sim_results(sim7_100, "DGP-7")
+  
+  # DGP 8
+  sim8_100_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 100)
+  sim8_100 = verify_sim_results(sim8_100_out)
+  verify_iteration_counts(sim8_100)
+  save_sim_results(sim8_100, "DGP-8")
+  
+  # Push new results
+  push_sim_results()
 }
+
 writeLines("Which simulation process should be used?")
 writeLines("-----------------------------------------------------------------")
 writeLines("Press 0 to load results from disk to replicate tables and figures")
@@ -15,217 +181,20 @@ sim_bool = utils::menu(c("Single-core (takes around 6h per simulation)",
 if (sim_bool == 0) {
   load_all_results()
 } else if (sim_bool == 3) {
-  ## Simulate using mclapply, save results and stop
   
-  ## 55 periods ##
-  # DGP 1
-  sim1_55_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 55)
-  sim1_55 = verify_sim_results(sim1_55_out)
-  verify_iteration_counts(sim1_55)
-  save_sim_results(sim1_55, "DGP-1")
-  
-  # DGP 2
-  sim2_55_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 55)
-  sim2_55 = verify_sim_results(sim2_55_out)
-  verify_iteration_counts(sim2_55)
-  save_sim_results(sim2_55, "DGP-2")
-  
-  # DGP 3
-  sim3_55_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 55)
-  sim3_55 = verify_sim_results(sim3_55_out)
-  verify_iteration_counts(sim3_55)
-  save_sim_results(sim3_55, "DGP-3")
-  
-  # DGP 4
-  sim4_55_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 55)
-  sim4_55 = verify_sim_results(sim4_55_out)
-  verify_iteration_counts(sim4_55)
-  save_sim_results(sim4_55, "DGP-4")
-  
-  # DGP 5
-  sim5_55_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 55)
-  sim5_55 = verify_sim_results(sim5_55_out)
-  verify_iteration_counts(sim5_55)
-  save_sim_results(sim5_55, "DGP-5")
-  
-  # DGP 6
-  sim6_55_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 55)
-  sim6_55 = verify_sim_results(sim6_55_out)
-  verify_iteration_counts(sim6_55)
-  save_sim_results(sim6_55, "DGP-6")
-  
-  # DGP 7
-  sim7_55_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 55)
-  sim7_55 = verify_sim_results(sim7_55_out)
-  verify_iteration_counts(sim7_55)
-  save_sim_results(sim7_55, "DGP-7")
-  
-  # DGP 8
-  sim8_55_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 55)
-  sim8_55 = verify_sim_results(sim8_55_out)
-  verify_iteration_counts(sim8_55)
-  save_sim_results(sim8_55, "DGP-8")
-  
-  
-  ## 100 periods ##
-  # DGP 1
-  sim1_100_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 100)
-  sim1_100 = verify_sim_results(sim1_100_out)
-  verify_iteration_counts(sim1_100)
-  save_sim_results(sim1_100, "DGP-1")
-  
-  # DGP 2
-  sim2_100_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 100)
-  sim2_100 = verify_sim_results(sim2_100_out)
-  verify_iteration_counts(sim2_100)
-  save_sim_results(sim2_100, "DGP-2")
-  
-  # DGP 3
-  sim3_100_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 100)
-  sim3_100 = verify_sim_results(sim3_100_out)
-  verify_iteration_counts(sim3_100)
-  save_sim_results(sim3_100, "DGP-3")
-  
-  # DGP 4
-  sim4_100_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 100)
-  sim4_100 = verify_sim_results(sim4_100_out)
-  verify_iteration_counts(sim4_100)
-  save_sim_results(sim4_100, "DGP-4")
-  
-  # DGP 5
-  sim5_100_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 100)
-  sim5_100 = verify_sim_results(sim5_100_out)
-  verify_iteration_counts(sim5_100)
-  save_sim_results(sim5_100, "DGP-5")
-  
-  # DGP 6
-  sim6_100_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 100)
-  sim6_100 = verify_sim_results(sim6_100_out)
-  verify_iteration_counts(sim6_100)
-  save_sim_results(sim6_100, "DGP-6")
-  
-  # DGP 7
-  sim7_100_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 100)
-  sim7_100 = verify_sim_results(sim7_100_out)
-  verify_iteration_counts(sim7_100)
-  save_sim_results(sim7_100, "DGP-7")
-  
-  # DGP 8
-  sim8_100_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 100)
-  sim8_100 = verify_sim_results(sim8_100_out)
-  verify_iteration_counts(sim8_100)
-  save_sim_results(sim8_100, "DGP-8")
-  
+  sim_all()
+
   # Push new results
   push_sim_results()
   stop("Simulations Done!")
   
 } else if (sim_bool == 2) {
-  ## Simulate using mclapply
-  
-  ## 55 periods ##
-  # DGP 1
-  sim1_55_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 55)
-  sim1_55 = verify_sim_results(sim1_55_out)
-  verify_iteration_counts(sim1_55)
-  save_sim_results(sim1_55, "DGP-1")
-  
-  # DGP 2
-  sim2_55_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 55)
-  sim2_55 = verify_sim_results(sim2_55_out)
-  verify_iteration_counts(sim2_55)
-  save_sim_results(sim2_55, "DGP-2")
-  
-  # DGP 3
-  sim3_55_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 55)
-  sim3_55 = verify_sim_results(sim3_55_out)
-  verify_iteration_counts(sim3_55)
-  save_sim_results(sim3_55, "DGP-3")
-  
-  # DGP 4
-  sim4_55_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 55)
-  sim4_55 = verify_sim_results(sim4_55_out)
-  verify_iteration_counts(sim4_55)
-  save_sim_results(sim4_55, "DGP-4")
-  
-  # DGP 5
-  sim5_55_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 55)
-  sim5_55 = verify_sim_results(sim5_55_out)
-  verify_iteration_counts(sim5_55)
-  save_sim_results(sim5_55, "DGP-5")
-  
-  # DGP 6
-  sim6_55_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 55)
-  sim6_55 = verify_sim_results(sim6_55_out)
-  verify_iteration_counts(sim6_55)
-  save_sim_results(sim6_55, "DGP-6")
-  
-  # DGP 7
-  sim7_55_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 55)
-  sim7_55 = verify_sim_results(sim7_55_out)
-  verify_iteration_counts(sim7_55)
-  save_sim_results(sim7_55, "DGP-7")
-  
-  # DGP 8
-  sim8_55_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 55)
-  sim8_55 = verify_sim_results(sim8_55_out)
-  verify_iteration_counts(sim8_55)
-  save_sim_results(sim8_55, "DGP-8")
-  
-  
-  ## 100 periods ##
-  # DGP 1
-  sim1_100_out = run_sim_parallel(1:505, dgp_1_sim, n = 500, t = 100)
-  sim1_100 = verify_sim_results(sim1_100_out)
-  verify_iteration_counts(sim1_100)
-  save_sim_results(sim1_100, "DGP-1")
-  
-  # DGP 2
-  sim2_100_out = run_sim_parallel(1:505, dgp_2_sim, n = 500, t = 100)
-  sim2_100 = verify_sim_results(sim2_100_out)
-  verify_iteration_counts(sim2_100)
-  save_sim_results(sim2_100, "DGP-2")
-  
-  # DGP 3
-  sim3_100_out = run_sim_parallel(1:505, dgp_3_sim, n = 500, t = 100)
-  sim3_100 = verify_sim_results(sim3_100_out)
-  verify_iteration_counts(sim3_100)
-  save_sim_results(sim3_100, "DGP-3")
-  
-  # DGP 4
-  sim4_100_out = run_sim_parallel(1:505, dgp_4_sim, n = 500, t = 100)
-  sim4_100 = verify_sim_results(sim4_100_out)
-  verify_iteration_counts(sim4_100)
-  save_sim_results(sim4_100, "DGP-4")
-  
-  # DGP 5
-  sim5_100_out = run_sim_parallel(1:505, dgp_5_sim, n = 500, t = 100)
-  sim5_100 = verify_sim_results(sim5_100_out)
-  verify_iteration_counts(sim5_100)
-  save_sim_results(sim5_100, "DGP-5")
-  
-  # DGP 6
-  sim6_100_out = run_sim_parallel(1:505, dgp_6_sim, n = 500, t = 100)
-  sim6_100 = verify_sim_results(sim6_100_out)
-  verify_iteration_counts(sim6_100)
-  save_sim_results(sim6_100, "DGP-6")
-  
-  # DGP 7
-  sim7_100_out = run_sim_parallel(1:505, dgp_7_sim, n = 500, t = 100)
-  sim7_100 = verify_sim_results(sim7_100_out)
-  verify_iteration_counts(sim7_100)
-  save_sim_results(sim7_100, "DGP-7")
-  
-  # DGP 8
-  sim8_100_out = run_sim_parallel(1:505, dgp_8_sim, n = 500, t = 100)
-  sim8_100 = verify_sim_results(sim8_100_out)
-  verify_iteration_counts(sim8_100)
-  save_sim_results(sim8_100, "DGP-8")
+  sim_all()
   
   # Push new results
   push_sim_results()
   
-  # load results
+  # load results for further analysis
   load_all_results()
   
 } else {
